b75085c3003025fa32b9390c8e9f3b8c287f7b10
Bhupesh-V <varshneybhupesh@gmail.com>
2019-08-10 19:53:53 +0530

    minor improvements - ported to PWA - add logo - add loading animation - add `<meta>` info

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ var repositories = [];
 var username;
 
 
+function addLoadSVG(){
+	loadsvg = document.getElementById("load");
+	setimg = document.createElement("img");
+	setimg.setAttribute("src", "puff.svg");
+	setimg.setAttribute("width", "40");
+	loadsvg.appendChild(setimg);
+}
+
+
+function deleteLoadSVG(){
+	var element = document.querySelector("#load");
+    element.style.display = 'none';
+}
+
+
 function getUsername() {
 	username = document.getElementById("username").value;
+	addLoadSVG();
 	getRepositories(username);
 }
 
@@ -41,25 +57,35 @@ function getIssues(){
 	}
 }
 
-
+/*function displayError(){
+	htmllist = document.getElementById("list");
+	var errorheading = document.createElement("h3");
+	var errortext = document.createTextNode("Ah! Looks like you currently don't have any open issues.");
+	errorheading.appendChild(errortext);
+	htmllist.appendChild(errorheading);
+	return;
+}
+*/
 function listOfIssues(issues){
-	for (var i=0;i<issues.length;i++){
+	deleteLoadSVG();
+	if (issues.length !== 0){
+		for (var i=0;i<issues.length;i++){
+			if (issues[i] != null) {
+				htmllist = document.getElementById("list");
+				var para = document.createElement("p");
 
-		if (issues[i] != null) {
-			htmllist = document.getElementById("list");
-			var para = document.createElement("p");
+				//create link
+				var issuelink = document.createElement("a");
+				issuelink.setAttribute("href", issues[i].html_url);
+				var issueTitle = document.createTextNode(issues[i].title);
+				issuelink.appendChild(issueTitle);
 
-			//create link
-			var issuelink = document.createElement("a");
-			issuelink.setAttribute("href", issues[i].html_url);
-			var issueTitle = document.createTextNode(issues[i].title);;
-			issuelink.appendChild(issueTitle);
-
-			//attach the link to the <p> tag
-			para.appendChild(issuelink);
+				//attach the link to the <p> tag
+				para.appendChild(issuelink);
 			
-			//attach the <p> tag to div = "list"
-			list.appendChild(para);
+				//attach the <p> tag to div = "list"
+				list.appendChild(para);
+			}
 		}
 	}
 }
