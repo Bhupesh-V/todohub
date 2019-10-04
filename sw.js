7b1a6c75957b42870d97fde51b629ff34033467c
vinodnimbalkar <omvinod@yahoo.com>
2019-10-04 18:18:20 +0530

    service worker added for offline web app

---
+++ sw.js
@@ -0,0 +1,29 @@
+const version = "0.1";
+const cacheName = `todohub-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/style.css`,
+        `/script.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
